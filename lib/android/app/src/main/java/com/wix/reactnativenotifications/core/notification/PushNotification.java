0009bb8d309322097a4cafd0d3b774863a57005a
juraam <juramoshkov@gmail.com>
2020-08-19 10:40:56 +0300

    Update PushNotification.java

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -4,10 +4,15 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.net.Uri;
 
 import com.facebook.react.bridge.ReactContext;
 import com.wix.reactnativenotifications.core.AppLaunchHelper;
@@ -59,15 +64,14 @@ public class PushNotification implements IPushNotification {
 
     @Override
     public void onReceived() throws InvalidNotificationException {
-        if (!mAppLifecycleFacade.isAppVisible()) {
-            postNotification(null);
-        }
+        postNotification(null);
         notifyReceivedToJS();
     }
 
     @Override
     public void onOpened() {
         digestNotification();
+        clearAllNotifications();
     }
 
     @Override
@@ -100,8 +104,6 @@ public class PushNotification implements IPushNotification {
 
         if (mAppLifecycleFacade.isAppVisible()) {
             dispatchImmediately();
-        } else if (mAppLifecycleFacade.isAppDestroyed()) {
-            launchOrResumeApp();
         } else {
             dispatchUponVisibility();
         }
@@ -151,7 +153,22 @@ public class PushNotification implements IPushNotification {
                 .setContentIntent(intent)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setAutoCancel(true);
-
+        String pushType = mNotificationProps.getPushType();
+        if (pushType.equals("START_SOS")) {
+            int resourceId = getAppResourceId("sos", "raw");
+            Uri soundUri = new Uri.Builder()
+                    .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                    .authority(mContext.getResources().getResourcePackageName(resourceId))
+                    .appendPath(mContext.getResources().getResourceTypeName(resourceId))
+                    .appendPath(mContext.getResources().getResourceEntryName(resourceId))
+                    .build();
+            try {
+                Ringtone r = RingtoneManager.getRingtone(mContext, soundUri);
+                r.play();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         setUpIcon(notification);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -194,6 +211,11 @@ public class PushNotification implements IPushNotification {
     protected void postNotification(int id, Notification notification) {
         final NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(id, notification);
+    }
+
+    protected void clearAllNotifications() {
+        final NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
     }
 
     protected int createNotificationId(Notification notification) {
